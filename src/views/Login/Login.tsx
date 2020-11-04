85bbcef5bae985144531190de39c4224dc6f25c8
jwwang2003 <jwwang.2003@gmail.com>
2020-11-04 15:58:44 -0800

    minor changes

--- src/views/Login/Login.tsx
+++ src/views/Login/Login.tsx
@@ -1,11 +1,37 @@
 import * as React from "preact/compat";
 import { useState, useEffect } from "preact/compat";
 
+import * as firebase from 'firebase/app';
+
 export default function Login() {
-  
+  const [email, setEmail] = useState<String>('');
+  const [password, setPassword] = useState<String>('');
+
+  function handleLogin() {
+    
+  }
+
+  function handleRegister() {
+
+  }
+
   return (
     <>
       <h1>Login?</h1>
+      <button onClick={() => googleSignIn()}>Login with google</button>
     </>
   )
 };
+
+function emailSignIn() {
+
+}
+
+function emailCreate() {
+  
+}
+
+function googleSignIn() {
+  const provider = new firebase.default.auth.GoogleAuthProvider();
+  firebase.default.auth().signInWithPopup(provider);
+}
--- src/views/Main/Main.tsx
+++ src/views/Main/Main.tsx
@@ -1,5 +1,4 @@
 import * as React from "preact/compat";
-import { useState, useEffect } from "preact/compat";
 
 export default function Main() {
   
